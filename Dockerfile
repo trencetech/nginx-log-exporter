de52d6cc9e82ba8a41e54ddbd8589229db2a730d
idoko <okokomichaels@outlook.com>
2025-05-28 23:50:11 +0100

    set up basic auth

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM --platform=$BUILDPLATFORM golang:1.24-alpine AS builder
+
+ARG TARGETOS
+ARG TARGETARCH
+
+RUN apk update && apk add --no-cache git bash
+
+WORKDIR /nginx-log-exporter
+COPY go.mod go.sum /nginx-log-exporter/
+RUN go get ./...
+ADD . /nginx-log-exporter
+
+RUN CGO_ENABLED=0 GOOS=$TARGETOS GOARCH=$TARGETARCH go build -buildvcs=false -o nginx-log-exporter
+
+FROM alpine:latest
+
+COPY --from=builder /nginx-log-exporter/nginx-log-exporter /nginx-log-exporter
+
+ARG BUILD_HASH
+ENV BUILD_HASH=$BUILD_HASH
+
+EXPOSE 9999
+ENTRYPOINT ["/nginx-log-exporter/nginx-log-exporter"]
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/subtle"
 	"flag"
 	"fmt"
 	"log"
@@ -15,10 +16,13 @@ import (
 
 func main() {
 	var listenAddress, configFile string
+	var username, password string
 	var placeholderReplace bool
 
 	flag.StringVar(&listenAddress, `web.listen-address`, `:9999`, `Address to listen on for the web interface and API.`)
 	flag.StringVar(&configFile, `config.file`, `config.yml`, `Nginx log exporter configuration file name.`)
+	flag.StringVar(&username, `web.username`, `jogodo`, `Nginx log exporter username for basic auth.`)
+	flag.StringVar(&password, `web.password`, `jegedee`, `Nginx log exporter password for basic auth.`)
 	flag.BoolVar(&placeholderReplace, `placeholder.replace`, false, `Enable placeholder replacement when rewriting the request path.`)
 	flag.Parse()
 
@@ -36,13 +40,36 @@ func main() {
 
 	fmt.Printf("running HTTP server on address %s\n", listenAddress)
 
-	http.Handle("/metrics", promhttp.HandlerFor(
+	metricsHandler := promhttp.HandlerFor(
 		prometheus.DefaultGatherer,
-		promhttp.HandlerOpts{
-			EnableOpenMetrics: true,
-		},
-	))
+		promhttp.HandlerOpts{},
+	)
+	protectedHandler := basicAuth(metricsHandler, username, password, "Metrics")
+	http.Handle("/metrics", protectedHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
 	if err := http.ListenAndServe(listenAddress, nil); err != nil {
 		log.Fatalf("start server with error: %v\n", err)
 	}
+}
+
+func basicAuth(handler http.Handler, username, password, realm string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 ||
+			subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorized.\n"))
+			return
+		}
+
+		handler.ServeHTTP(w, r)
+	})
 }
